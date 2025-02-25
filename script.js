3add6140b3be905a875e7980b67eecf93d648d04
RaccoonStampede <Daedaehudson@gmail.com>
2025-02-24 23:08:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -80,4 +80,65 @@ function resetJob() {
 document.addEventListener('DOMContentLoaded', () => {
     loadProgress();
     showPage('main-page');
-});
+});const predefinedSteps = {
+    Flare: [
+        "New location, flare located at far end of most pad sites",
+        "Reinstall 3/4\" conduit from flare disconnect to location of flare VFD panel",
+        "Install a guat box on conduit and continue conduit run to the end of flare pipe rack",
+        "Stack, add flex from guat to panel and pull wire through conduit to guat box with enough wire to go",
+        "Install 3/4\" flex from guat box to SPC panel or other type of flare VFD panel",
+        "Install 3/4\" flex from end of conduit run to flare blower motor",
+        "Pull wire (#10 THHN, Brown/Orange/Yellow/Green) from flare disconnect on panel rack to guat box, VFD panel",
+        "Pull wire from flare panel to flare blower motor and terminate",
+        "Terminating wires - flare panel wires (coming from disconnect) will terminate on P side of breaker inside VFD",
+        "Blower motor wires will terminate on the drive inside panel, will have to remove covers to access",
+        "Flare blower motor will be terminated (L1-Brown) as indicated on nameplate"
+    ],
+    Igniter: [
+        "Igniter control box (red plastic box) usually mounted on or near flare",
+        "VFD panel mounted on or near flare",
+        "Reinstall 3/4\" flex from VFD panel to igniter panel",
+        "Pull wire (#10 THHN, Black/White/Green) from VFD panel to igniter panel",
+        "From VFD panel wires to igniter panel, terminate on low voltage control transformer",
+        "Igniter panel wires terminate on power supply terminals marked L1, N, Ground",
+        "Igniter on new flare stack",
+        "Wiring zone prewired on flare stack",
+        "Open round J-box and check wiring colors (Red/Yellow tied to L, Black/White tied to N)",
+        "Run cable down conduit from back to igniter control panel and install into panel",
+        "Terminate wires to green blocks on the inside of door panel labeled XF Igniter (L+R), match colors with cable terminations on J-box on flare stack",
+        "Terminate last two wires on terminals marked TEMP"
+    ],
+    BottomsPump: [
+        "Bottoms pump (circulation pump), location varies",
+        "Get needed 3/4\" of 2\" rigid conduit, install conduit from panel rack to location of bottoms pump",
+        "Install 3/4\" flex from conduit to bottoms pump panel",
+        "Cut 2 pieces of unistrut at 5', use 2 exhaust clamps to mount unistrut to leg of bottoms pump stand",
+        "Cut 5' of 2\" rigid conduit, mount conduit on unistrut with 2\" unistrut straps, cement poles",
+        "Use bottoms pump panel, needs holes",
+        "Drill 4 holes in mounting brackets of bottoms pump panel, use exhaust clamps for hole reference (2 holes on top and 2 holes on bottom)",
+        "Mount pump panel to pole using exhaust clamps",
+        "Install 3/4\" flex from the end of conduit run to the bottom of pump panel",
+        "Pull wire (#10 THHN, Brown/Yellow/Black/Green) from flare disconnect to pump panel rack and terminate at the top of disconnect inside panel",
+        "Install 3/4\" flex from panel to pump motor on stand"
+    ],
+    AirCompressor: [
+        "Air compressor, install 3/4\" conduit from air compressor disconnect to air compressor VFD on power panel rack",
+        "Pull wire (#8/#8/#8/#10 THHN) from disconnect to disconnect",
+        "Install 3/4\" flex from the end of conduit to the bottom of disconnect on skid",
+        "Terminate wires on top side of disconnect on air compressor skid"
+    ],
+    SaltWaterPump: [
+        "Salt water pump (SWP)",
+        "Reinstall 1\" conduit from saltwater pump disconnect to location of SWP VFD",
+        "Install 1\" flex from conduit to VFD panel",
+        "Pull wire from disconnect on power panel rack to VFD panel and terminate wires on top side of disconnect breaker in panel",
+        "Install 1\" flex from VFD to SWP motor",
+        "Pull wire from VFD to pump motor and terminate in accordance with name plate on motor",
+        "Terminate N wires on VFD pump panel on the bottom of VFD (remove cover)"
+    ],
+    '120VSystem': [
+        "Placeholder: Install 120V wiring from panel to equipment",
+        "Placeholder: Test 120V connections",
+        "Placeholder: Verify voltage at equipment"
+    ]
+};
